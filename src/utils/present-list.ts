3eb011a142c69eea2d0e4d117086165b6feb10c3
Dominic Ruggiero <dominic@userexe.me>
2023-02-14 21:11:37 -0500

    classic reversed layout

--- src/utils/present-list.ts
+++ src/utils/present-list.ts
@@ -7,6 +7,13 @@ const presentScreens: presentScreen[] = [
     slug: "classic",
   },
   {
+    name: "Classic reversed",
+    author: "Dominic",
+    authorLink: "https://github.com/mrhappyma",
+    description: "The original score screen, with the gradient reversed",
+    slug: "classic-reversed",
+  },
+  {
     name: "Micah's version",
     author: "Dominic (design by Micah Carlson)",
     authorLink: "https://micah.page",
